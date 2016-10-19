8d79bfcd0f5480be6440dee9396413f4f9a5d8cd
croot <forever90051@gmail.com>
2016-10-20 00:05:15 +0900

    added ignore list

--- grunt/jsbeautifier.js
+++ grunt/jsbeautifier.js
@@ -3,16 +3,9 @@ module.exports = {
 		config: "<%= path.conf %>/.jsbeautifyrc"
 	},
 	dev: {
-<<<<<<< HEAD
 		src: ["<%= path.build %>/**/*.{js,json,html}","!**/*.min.js"],
 	},
 	dist: {
 		src: ["<%= path.tmpdist %>/**/*.{js,json,html}","!**/*.min.js"],
-=======
-		src: ['<%= path.tmpdev %>**/*.{js,json,html}','!**/*.min.js'],
-	},
-	dist: {
-		src: ['<%= path.tmpdist %>**/*.{js,json,html}','!**/*.min.js'],
->>>>>>> 02d02afb2b3ac19eccbaa2151185db1535a6468c
 	}
 }
